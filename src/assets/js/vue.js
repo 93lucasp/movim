6d7cd090efdcde898b9a09ceb5a1e9b489862093
LucaS <spezzanoluca@gmail.com>
2017-11-12 18:04:01 +0100

    finished hero and added coponent

--- src/assets/js/vue.js
+++ src/assets/js/vue.js
@@ -1,7 +1,8 @@
-var itemsNavbar = [{
-        text: 'Chi siamo',
-        link: 'chisiamo.html',
-        class: 'menu__chiSiamo'
+var itemsNavbar = [
+    {
+        text: 'Home',
+        link: 'home.html',
+        class: 'menu__home'
     },
     {
         text: 'Rent',
@@ -30,11 +31,44 @@ var itemsNavbar = [{
     }
 ];
 
+var heros = {
+    home: {
+        title: 'Il Futuro per',
+        titleEvidence: 'la tua impresa',
+        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    green: {
+        title: 'Il Futuro per la tua impresadasddasddasd',
+        subTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    }};
 
 
 Vue.component('vue-item', {
     props: ['item'],
     template: '<li v-bind:class="item.class">  <a v-bind:href="item.link"> {{ item.text }} </a>  </li>'
+});
+
+Vue.component('vue-hero', {
+    props: ['hero'],
+    template: 
+    '<div>\
+        <div class="container">\
+            <div class="row">\
+                <div class="col-lg-4 mx-lg-auto">\
+                    <h1 class="font-weight-light">{{hero.title}} <span class="font-weight-bold"> {{hero.titleEvidence}}</span></h1>\
+                    <p>{{hero.subTitle}}</p>\
+                    <div class="lineBold mt-2"></div>\
+                </div>\
+            </div>\
+        </div>\
+    </div>'
+});
+
+new Vue({
+    el: '#hero',
+    data: {
+        heros: heros
+    }
 });
 
 new Vue({
